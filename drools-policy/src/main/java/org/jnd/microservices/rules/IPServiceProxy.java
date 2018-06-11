67604ffc37eeedb5371b39200a5e08960530a68a
Justin Davis <justinndavis@gmail.com>
2018-06-11 12:00:12 +0100

    drools policy stuff

--- drools-policy/src/main/java/org/jnd/microservices/rules/IPServiceProxy.java
+++ drools-policy/src/main/java/org/jnd/microservices/rules/IPServiceProxy.java
@@ -24,26 +24,24 @@ public class IPServiceProxy {
         log.info("IPServiceProxy : validate address : " + address.getAddress());
 
         try {
-            log.info("IPServiceProxy : 1");
+
             HttpEntity<IPAddress> request = new HttpEntity(address);
-            log.info("IPServiceProxy : 2");
+
             ResponseEntity<IPAddress> exchange =
                     this.restTemplate.exchange(
                             base_url + "/ip/validate",
                             HttpMethod.POST,
                             request,
                             IPAddress.class);
-            log.info("IPServiceProxy : 3");
+
             address = exchange.getBody();
-            log.info("IPServiceProxy : 4");
-            address.setGranted(true);
+
         }
         catch (HttpClientErrorException hcee)   {
-            log.info("IPServiceProxy : 5");
             //do nothing this user does not exist, the 404 causes the exception to be thrown
             log.info("IPServiceProxy address not valid : " + address.getAddress());
         }
-        log.info("IPServiceProxy : 6");
+
         return address;
     }
 
