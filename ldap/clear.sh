186d794d7a78889715943a50361ec8311845aa7b
Justin Davis <justinndavis@gmail.com>
2018-10-15 21:06:44 +0100

    signing stuff

--- ldap/clear.sh
+++ ldap/clear.sh
@@ -11,6 +11,8 @@ tac dns.txt > snd.txt
 while read dn;
 do
     echo Deleting : $dn;
+    dn=$(echo $dn | sed 's/dn: //')
+    echo Deleting : $dn;
     ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "$dn"
 done < snd.txt
 
