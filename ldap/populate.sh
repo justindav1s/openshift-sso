bc8b4aaccd623cc78e9ab5bbd47328f853031f9c
Justin Davis <justinndavis@gmail.com>
2018-10-15 23:02:26 +0100

    ldap

--- ldap/populate.sh
+++ ldap/populate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ldapadd -x -W -D "cn=ldapadm,dc=datr,dc=eu" -f base.ldif
+ldapadd -x -w 12jnd34 -D "cn=ldapadm,dc=datr,dc=eu" -f base.ldif
 
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Justin Davis,ou=people,dc=datr,dc=eu"
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Super Man,ou=people,dc=datr,dc=eu"
