f705399d311779118b5e7994458e2c2b38c18f39
Justin Davis <justinndavis@gmail.com>
2018-10-15 21:01:27 +0100

    signing stuff

--- ldap/clear.sh
+++ ldap/clear.sh
@@ -5,11 +5,13 @@ PASSWORD=$1
 
 ldapsearch  -w $PASSWORD -D "cn=ldapadm,dc=datr,dc=eu" -b "dc=datr,dc=eu" "*" | grep dn > dns.txt
 
+tac dns.txt > snd.txt
+
 while read dn;
 do
     echo $dn;
     ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" $dn
-done < dns.txt
+done < snd.txt
 
 
 ldapsearch  -w $PASSWORD -D "cn=ldapadm,dc=datr,dc=eu" -b "dc=datr,dc=eu" "*"
