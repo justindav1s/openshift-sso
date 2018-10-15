00fe10e0a6ec4045298698b5d552c41b82c3deae
Justin Davis <justinndavis@gmail.com>
2018-10-15 23:09:56 +0100

    ldap

--- ldap/populate.sh
+++ ldap/populate.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+PASSWORD=$1
 
-ldapadd -x -w 12jnd34 -D "cn=ldapadm,dc=datr,dc=eu" -f base.ldif
+ldapadd -x -w $PASSWORD -D "cn=ldapadm,dc=datr,dc=eu" -f base.ldif
 
 echo setting passwords
 
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Justin Davis,ou=people,dc=datr,dc=eu"
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Super Man,ou=people,dc=datr,dc=eu"
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=June Rossi,ou=people,dc=datr,dc=eu"
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Marc Chambers,ou=people,dc=datr,dc=eu"
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Robert Wong,ou=people,dc=datr,dc=eu"
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Cat Woman,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w $PASSWORD -s password "cn=Justin Davis,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w $PASSWORD -s password "cn=Super Man,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w $PASSWORD -s password "cn=June Rossi,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w $PASSWORD -s password "cn=Marc Chambers,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w $PASSWORD -s password "cn=Robert Wong,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w $PASSWORD -s password "cn=Cat Woman,ou=people,dc=datr,dc=eu"
 
