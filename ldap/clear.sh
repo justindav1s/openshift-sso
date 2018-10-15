fe9a14579dee65d558e5d7d976649a25bb2941ea
Justin Davis <justinndavis@gmail.com>
2018-10-15 19:53:35 +0100

    signing stuff

--- ldap/clear.sh
+++ ldap/clear.sh
@@ -4,18 +4,22 @@ PASSWORD=$1
 
 ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=justindav1s,ou=people,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "cn=Marc Chambers,ou=people,dc=datr,dc=eu"
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=marc,ou=people,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "cn=June Rossi,ou=people,dc=datr,dc=eu"
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=rossi,ou=people,dc=datr,dc=eu"
 
 ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=superman,ou=people,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=justindav1s,ou=people,dc=datr,dc=eu"
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=wong,ou=people,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "cn=Robert Wong,ou=people,dc=datr,dc=eu"
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=marketing,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=people, dc=datr,dc=eu"
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=accounting,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=groups,dc=datr,dc=eu"
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=superhero,dc=datr,dc=eu"
+
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=people,dc=datr,dc=eu"
+
+ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=leadership,dc=datr,dc=eu"
 
 ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "dc=datr,dc=eu"
