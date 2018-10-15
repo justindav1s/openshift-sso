e9577577c9d2eee8d4b943503376e29d7493acbf
Justin Davis <justinndavis@gmail.com>
2018-10-15 23:08:26 +0100

    ldap

--- ldap/populate.sh
+++ ldap/populate.sh
@@ -8,7 +8,7 @@ echo setting passwords
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Justin Davis,ou=people,dc=datr,dc=eu"
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Super Man,ou=people,dc=datr,dc=eu"
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=June Rossi,ou=people,dc=datr,dc=eu"
-ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=cn=Marc Chambers,ou=people,dc=datr,dc=eu"
+ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Marc Chambers,ou=people,dc=datr,dc=eu"
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Robert Wong,ou=people,dc=datr,dc=eu"
 ldappasswd -H ldap://127.0.0.1:389 -D "cn=ldapadm,dc=datr,dc=eu" -w 12jnd34 -s password "cn=Cat Woman,ou=people,dc=datr,dc=eu"
 
