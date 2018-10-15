08a8e22abcf440090c936ae7cd8ea0704d5a94ac
Justin Davis <justinndavis@gmail.com>
2018-10-15 20:58:52 +0100

    signing stuff

--- ldap/clear.sh
+++ ldap/clear.sh
@@ -2,24 +2,14 @@
 
 PASSWORD=$1
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=justindav1s,ou=people,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=marc,ou=people,dc=datr,dc=eu"
+ldapsearch  -w $PASSWORD -D "cn=ldapadm,dc=datr,dc=eu" -b "dc=datr,dc=eu" "*" | grep dn > dns.txt
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=rossi,ou=people,dc=datr,dc=eu"
+while read dn;
+do
+    echo $dn;
+    ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" $dn
+done < dns.txt
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=superman,ou=people,dc=datr,dc=eu"
 
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "uid=wong,ou=people,dc=datr,dc=eu"
-
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=marketing,dc=datr,dc=eu"
-
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=accounting,dc=datr,dc=eu"
-
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=superhero,dc=datr,dc=eu"
-
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=people,dc=datr,dc=eu"
-
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "ou=leadership,dc=datr,dc=eu"
-
-ldapdelete -w ${PASSWORD} -D "cn=ldapadm,dc=datr,dc=eu" "dc=datr,dc=eu"
+ldapsearch  -w $PASSWORD -D "cn=ldapadm,dc=datr,dc=eu" -b "dc=datr,dc=eu" "*"
